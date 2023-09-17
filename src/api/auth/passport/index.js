69c63b022a946293d3d2e3ad8678bf4590cfae72
Lefju <karol.legut121@gmail.com>
2023-09-17 11:00:12 +0200

    feat: +github

--- src/api/auth/passport/index.js
+++ src/api/auth/passport/index.js
@@ -1,5 +1,7 @@
 const googlePassport = require('../passport/google.passport');
+const githubPassport = require('../passport/github.passport');
 
 module.exports = {
     googlePassport,
+    githubPassport,
 };
