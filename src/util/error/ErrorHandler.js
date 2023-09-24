95e42b6e71b47ee87caff8685a709a7f02d23ce4
Lefju <karol.legut121@gmail.com>
2023-09-24 10:55:42 +0200

    feat: error handler SequelizeForeignKeyConstraint

--- src/util/error/ErrorHandler.js
+++ src/util/error/ErrorHandler.js
@@ -14,10 +14,11 @@ const handleDuplicateFieldsDB = (err) => {
 };
 
 const handleValidationErrorDB = (err) => {
-    const errors = Object.values(err.errors).map((el) => el.message);
+    console.log(err.parent.detail);
+    // const errors = Object.values(err.errors).map((el) => el.message);
 
-    const message = `Invalid input data. ${errors.join('. ')}`;
-    return new AppError(message, 400);
+    // const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(err.parent.detail, 400);
 };
 
 const handleJWTError = () =>
@@ -84,7 +85,7 @@ module.exports = (err, req, res, next) => {
 
         if (error.name === 'CastError') error = handleCastErrorDB(error);
         if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-        if (error.name === 'ValidationError')
+        if (error.name === 'SequelizeForeignKeyConstraintError')
             error = handleValidationErrorDB(error);
         if (error.name === 'JsonWebTokenError') error = handleJWTError();
         if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
