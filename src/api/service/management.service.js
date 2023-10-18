cba4d91c261647c6fa33b4cad1d35f873153ad32
Lefju <karol.legut121@gmail.com>
2023-10-18 21:17:44 +0200

    fix: employee count, not all users

--- src/api/service/management.service.js
+++ src/api/service/management.service.js
@@ -1,7 +1,15 @@
 const { Task, List, User } = require('../model');
+const { roleEnum } = require('../model/role.enum');
+const { Op } = require('sequelize');
 
 exports.getStats = async () => {
-    const employeeCount = await User.count();
+    const employeeCount = await User.count({
+        where: {
+            role: {
+                [Op.eq]: roleEnum.EMPLOYEE,
+            },
+        },
+    });
     const taskCount = await Task.count();
     const listCount = await List.count();
 
