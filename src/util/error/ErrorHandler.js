4d607d87fa1ae18a5e34f8242cf1e7a632353a6a
Lefju <karol.legut121@gmail.com>
2023-10-28 18:14:12 +0200

    patch: error handler

--- src/util/error/ErrorHandler.js
+++ src/util/error/ErrorHandler.js
@@ -1,32 +1,6 @@
 const { NODE_ENV } = require('../../config');
 const AppError = require('./AppError');
 
-const handleCastErrorDB = (err) => {
-    const message = `Invalid ${err.path}: ${err.value}.`;
-    return new AppError(message, 400);
-};
-
-const handleDuplicateFieldsDB = (err) => {
-    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-
-    const message = `Duplicate field value: ${value}. Please use another value!`;
-    return new AppError(message, 400);
-};
-
-const handleValidationErrorDB = (err) => {
-    console.log(err.parent.detail);
-    // const errors = Object.values(err.errors).map((el) => el.message);
-
-    // const message = `Invalid input data. ${errors.join('. ')}`;
-    return new AppError(err.parent.detail, 400);
-};
-
-const handleJWTError = () =>
-    new AppError('Invalid token. Please log in again!', 401);
-
-const handleJWTExpiredError = () =>
-    new AppError('Your token has expired! Please log in again.', 401);
-
 const sendErrorDev = (err, req, res) => {
     if (req.originalUrl.startsWith('/api')) {
         return res.status(err.statusCode).json({
@@ -82,13 +56,6 @@ module.exports = (err, req, res, next) => {
     } else if (NODE_ENV === 'production') {
         let error = { ...err };
         error.message = err.message;
-
-        if (error.name === 'CastError') error = handleCastErrorDB(error);
-        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-        if (error.name === 'SequelizeForeignKeyConstraintError')
-            error = handleValidationErrorDB(error);
-        if (error.name === 'JsonWebTokenError') error = handleJWTError();
-        if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
         sendErrorProd(error, req, res);
     }
